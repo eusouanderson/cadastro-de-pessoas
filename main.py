fde064948516ed58814d604fca2ae1894e61ce1b
Anderson Rodrigues <eusouanderson@outlook.com>
2022-12-12 11:20:59 -0300

    "Create Layout"

---
+++ main.py
@@ -0,0 +1,53 @@
+import tkinter as tk
+from tkinter import ttk
+cont = 0
+class App(tk.Tk):
+
+    def __init__(self):
+        super().__init__()
+        self.title('Tkinter StringVar')
+        self.geometry("300x80")
+
+        self.name_var = tk.StringVar()
+
+        self.columnconfigure(0, weight=1)
+        self.columnconfigure(1, weight=1)
+        self.columnconfigure(2, weight=1)
+
+        self.create_widgets()
+
+    def create_widgets(self):
+
+        padding = {'padx': 5, 'pady': 5}
+        # label
+        ttk.Label(self, text='Name:').grid(column=0, row=0, **padding)
+
+        # Entry
+        name_entry = ttk.Entry(self, textvariable=self.name_var)
+        name_entry.grid(column=1, row=0, **padding)
+        name_entry.focus()
+
+        # Button
+        submit_button = ttk.Button(self, text='Submit', command=self.submit)
+        submit_button.grid(column=2, row=0, **padding)
+
+        # Output label
+        self.output_label = ttk.Label(self)
+        self.output_label.grid(column=0, row=1, columnspan=3, **padding)
+
+    cont = 0
+    def submit(self):
+        global cont
+        cont += 1
+        padding = {'padx': 5, 'pady': 5}
+
+        while True:
+            self.output_label.config(text=self.name_var.get())
+            self.output_label = ttk.Label(self)
+            self.output_label.grid(column=0, row=cont, columnspan=3, **padding)
+            self.name_var.set('')
+            break
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
